da428f8c5e968eaffdd3558d198eb8b97e56deb8
vladstanoiu <55551799+vladstanoiu@users.noreply.github.com>
2019-10-26 00:28:27 +0300

    PointeriSpreHeap

---
+++ pointeriSpreHeap/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+ double* pntA = new double (1.3);
+
+int main()
+{
+
+    double* pnt1 = new double (2.3);
+    double* pnt2 = new double (5.4);
+    double* pntMedie = new double;
+    *pntMedie = (*pntA + *pnt1 + *pnt2)/3;
+    cout << "Media este: " << *pntMedie << endl;
+    delete pntA,pnt1,pnt2,pntMedie;
+    pnt1 = NULL;
+    pntA = NULL;
+    pnt2 = NULL;
+    pntMedie =NULL;
+
+    return 0;
+}
