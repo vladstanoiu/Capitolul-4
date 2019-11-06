559a8a4b78064341baf2857054e2c34cf969a719
vladstanoiu <55551799+vladstanoiu@users.noreply.github.com>
2019-11-06 11:16:38 +0200

    Update main.cpp

--- adrese/Structuri/main.cpp
+++ adrese/Structuri/main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 struct Adresa{
 
- char prenume[10] = "Vlad", strada[20] = "Luceafarul", judet[10] = "Timis", oras[15] = "Timisoara";
+ char prenume[10], strada[20], judet[10], oras[15];
  int numar;
  int codPostal;
     };
@@ -12,8 +12,17 @@ struct Adresa{
 int main()
 {
     Adresa adresa;
+    adresa.prenume;
     adresa.numar = 0;
+    adresa.oras;
+    adresa.judet;
+    adresa.strada;
     adresa.codPostal = 300414;
+    cout << "Introduceti prenumele , judetul, orasul si strada dvs. in aceasta ordine: " << endl;
+    cin >> adresa.prenume;
+    cin >> adresa.judet;
+    cin >> adresa.oras;
+    cin >> adresa.strada;
     cout << "Ma numesc " << adresa.prenume << endl;
     cout << "M-am nascut in judetul " << adresa.judet << ", orasul " << adresa.oras << ", pe strada "<< adresa.strada << " la numarul " << adresa.numar << ". Ah, am uitat si codul postal :) -> " << adresa.codPostal << endl;
 
