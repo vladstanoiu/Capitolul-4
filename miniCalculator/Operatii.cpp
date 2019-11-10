9fc306994086b6fdc414d70996ee5a2528a8a97c
vladstanoiu <55551799+vladstanoiu@users.noreply.github.com>
2019-11-10 23:01:18 +0200

    MiniCalculator

---
+++ miniCalculator/Operatii.cpp
@@ -0,0 +1,17 @@
+
+int Adunare (int a,  int b){
+    return (a+b);}
+
+int Scadere ( int a, int b ){
+    return (a-b);}
+
+int Inmultire ( int a , int b){
+    return (a*b);}
+
+int Impartire ( int a , int b){
+    return (a / b);}
+
+int Modulo ( int a , int b ){
+    return (a % b);}
+
+
---
+++ miniCalculator/Operatii.h
@@ -0,0 +1,11 @@
+#ifndef OPERATII_H_INCLUDED
+#define OPERATII_H_INCLUDED
+
+ int Adunare ( int a  , int b );
+ int Scadere ( int a, int b);
+ int Inmultire ( int a , int b);
+ int Impartire ( int a , int b);
+ int Modulo ( int a , int b );
+
+
+#endif // OPERATII_H_INCLUDED
