a9fd1c6d08f9582f2af371f49a73d4830d053eca
vladstanoiu <55551799+vladstanoiu@users.noreply.github.com>
2019-11-10 23:21:42 +0200

    Update main.cpp

--- parametriiBATAiVINA/main.cpp
+++ parametriiBATAiVINA/main.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-void Ameteala ( int parSiVal , int& parTyRef , int * parTotPont ){
+void ameteala ( int parSiVal , int& parTyRef , int * parTotPont ){
 
     parSiVal *= 3;
     parTyRef *= 3;
@@ -14,7 +14,7 @@ int main()
 {
     int drunkenRat = 36;
     //trebuie sa initializam variabila cu 36 pentru ca rezultatul sa fie 216;
-    Ameteala(drunkenRat, drunkenRat, &drunkenRat);
+    ameteala(drunkenRat, drunkenRat, &drunkenRat);
     cout << drunkenRat ;
 
     return 0;
